16cf658016d378d6b309c490fe6879b428ae477e
Liudmila <lidiay@yandex.ru>
2022-11-28 15:13:12 +0300

    Дополнительный комит к п.4 Программа - вариант с вводимым массивом

--- Program.cs
+++ Program.cs
@@ -8,10 +8,41 @@
 ["1234","1567","-2","computer science"] -> ["-2]
 ["Russia","Denmark","Kazan"] -> []
 */
-
+// Вариант 1 - массив задан на старте выполнения алгоритма
+/*
 string[] array = {"Russia", "Denmark", "Kazan"};
 Console.WriteLine($"Первоначальный массив [{String.Join(", ", array)}]");
 Console.WriteLine($"Новый массив [{String.Join(", ", CreateArray(array))}]");
+
+string[] CreateArray (string[] arr)
+{
+    int len = new Random().Next(0,4);
+    string[] newArr = new string[len];
+    for (int i = 0; i < len; i++)
+    {
+        int j = new Random().Next(0,len);
+        newArr[i] = arr[j];
+    }
+    return newArr;
+}
+*/
+// Вариант 2 - массив вводится с клавиатуры
+Console.Write("Введите длину первоначального массива: ");
+int N = int.Parse(Console.ReadLine()!);
+string[] array = new string[N];
+FillArray(array);
+Console.WriteLine($"Первоначальный массив [{String.Join(", ", array)}]");
+Console.WriteLine($"Новый массив [{String.Join(", ", CreateArray(array))}]");
+
+void FillArray (string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"Введите {i + 1}-й символ/набор символов для массива: ");
+        string S = Console.ReadLine()!;
+        arr[i] = S;
+    }
+}
 
 string[] CreateArray (string[] arr)
 {
